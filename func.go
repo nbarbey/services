e7c71b2d4615ba07ff94d87e04504d3d1318eef4
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2024-01-05 10:17:23 +0100

    add func with goroutine

--- func.go
+++ func.go
@@ -5,3 +5,11 @@ type ServiceFunc func()
 func (f *ServiceFunc) Run() {
 	(func())(*f)()
 }
+
+type ServiceFuncGoRoutine func()
+
+func (f *ServiceFuncGoRoutine) Run() {
+	go func() {
+		(*ServiceFunc)(f).Run()
+	}()
+}
--- func_test.go
+++ func_test.go
@@ -3,6 +3,7 @@ package services
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
 )
 
 func TestHelloService(t *testing.T) {
@@ -12,4 +13,14 @@ func TestHelloService(t *testing.T) {
 	})
 	f.Run()
 	assert.True(t, out)
+}
+
+func TestHelloService_start_a_goroutine(t *testing.T) {
+	out := false
+	f := ServiceFuncGoRoutine(func() {
+		time.Sleep(1 * time.Millisecond)
+		out = true
+	})
+	f.Run()
+	assert.Eventually(t, func() bool { return out }, time.Second, time.Millisecond)
 }
