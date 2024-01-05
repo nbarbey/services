47277398ffcb6fbfee4d4d1feda65c04f333b0b9
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2024-01-05 11:44:29 +0100

    add example with graceful shutdown of multiple services

--- services_test.go
+++ services_test.go
@@ -3,6 +3,9 @@ package services
 import (
 	"context"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
+	"os/signal"
 	"testing"
 	"time"
 )
@@ -24,4 +27,22 @@ func TestServices_Run(t *testing.T) {
 
 	assert.Eventually(t, func() bool { return *o1 }, time.Second, time.Millisecond)
 	assert.Eventually(t, func() bool { return *o2 }, time.Second, time.Millisecond)
+}
+
+func TestServices_signal_interruption(t *testing.T) {
+	f1, out1 := makeCancellableSleeper(100, time.Millisecond)
+	f2, out2 := makeCancellableSleeper(100, time.Millisecond)
+	services := Services{f1, f2}
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	services.Run(ctx)
+
+	// simulate a SIGINT by sending signal to self
+	p, err := os.FindProcess(os.Getpid())
+	require.NoError(t, err)
+	require.NoError(t, p.Signal(os.Interrupt))
+
+	assert.Eventually(t, func() bool { return *out1 }, time.Second, 10*time.Millisecond)
+	assert.Eventually(t, func() bool { return *out2 }, time.Second, 10*time.Millisecond)
 }
