ac2be78f3c42eefea42fe8d6b6fcc06a822533e8
Nicolas Barbey <nicolas.a.barbey@gmail.com>
2024-01-08 10:59:42 +0100

    example http service

---
+++ example_http_test.go
@@ -0,0 +1,43 @@
+package services_test
+
+import (
+	"context"
+	"fmt"
+	"github.com/nbarbey/services"
+	"io"
+	"log"
+	"net/http"
+)
+
+func Example_http() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		_, err := io.WriteString(w, "hello")
+		if err != nil {
+			log.Fatalf("unable to write string: %s", err)
+		}
+	})
+	service := services.ServiceFuncGoRoutine(func(ctx context.Context) {
+		log.Fatal(http.ListenAndServe(":1234", nil))
+	})
+
+	service.Run(context.Background())
+	defer service.Stop(context.Background())
+
+	resp, err := http.Get("http://localhost:1234/hello")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("unexpected error code: %s", resp.Status)
+	}
+	defer func() { _ = resp.Body.Close() }()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp.Status)
+	fmt.Println(string(body))
+	// Output:
+	// 200 OK
+	// hello
+}
